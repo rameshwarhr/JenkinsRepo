4fd842cc75e94173d17a3fe77558d89ef1e097ea
rameshwarhr <rameshwarhr@yahoo.com>
2024-10-02 12:47:56 +0530

    update lib

--- src/main/java/DataProvider/dataProvider.java
+++ src/main/java/DataProvider/dataProvider.java
@@ -8,9 +8,7 @@ public class dataProvider {
 	public Object[][] loginTestData() {
 		return new Object[][] { { "standard_user", "secret_sauce" }, 
 				{ "problem_user", "secret_sauce" }, 
-				{ "performance_glitch_user", "secret_sauce" },
-				{ "error_user", "secret_sauce" }, 
-				{ "visual_user", "secret_sauce" } 
 				};
 	}
 }
+
--- src/main/java/Factory/BrowserFactory.java
+++ src/main/java/Factory/BrowserFactory.java
@@ -4,23 +4,38 @@ import java.time.Duration;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 import org.yaml.snakeyaml.scanner.Constant;
 
 import Pages.HomePage;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import utility.constant;
 
 public class BrowserFactory {
 
 	public static WebDriver driver;
 
+	@Parameters("Browser")
 	@BeforeMethod()
-	public void launchBrowser() {
-		System.setProperty("webdriver.chrome.driver", constant.chromePath);
-		driver = new ChromeDriver();
+	public void launchBrowser(String brwoserName) {
+		
+		if (brwoserName.equalsIgnoreCase("chrome")) {
+			WebDriverManager.chromedriver().setup();
+			driver = new ChromeDriver();
+		} else if (brwoserName.equalsIgnoreCase("firefox")) {
+			WebDriverManager.firefoxdriver().setup();
+			driver = new FirefoxDriver();
+		} else if (brwoserName.equalsIgnoreCase("edge")) {
+			WebDriverManager.edgedriver().setup();
+			driver = new EdgeDriver();
+		}
+
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		driver.get("https://www.saucedemo.com/");
@@ -32,7 +47,5 @@ public class BrowserFactory {
 		Thread.sleep(500);
 		driver.quit();
 	}
-
-
 
 }
